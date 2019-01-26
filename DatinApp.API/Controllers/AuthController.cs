00697db1340583ff049a421ae31ae705bf4ce30e
bulat <m11e@yandex.ru>
2019-01-26 14:14:22 +0300

    login and jwt token added

--- DatinApp.API/Controllers/AuthController.cs
+++ DatinApp.API/Controllers/AuthController.cs
@@ -1,22 +1,31 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatinApp.API.Data;
 using DatinApp.API.Dtos;
 using DatinApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatinApp.API.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    //[ApiController]
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository authRepository)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository authRepository, IConfiguration config)
         {
             _repo = authRepository;
+            _config = config;
         }
 
         [HttpPost]
+        [Route("register")]
         public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
         {
             //validate user
@@ -31,9 +40,41 @@ namespace DatinApp.API.Controllers
             };
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
-            
+
             return StatusCode(201);
         }
 
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody]UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]{
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+        }
     }
 }
--- DatinApp.API/Dtos/UserForLoginDto.cs
+++ DatinApp.API/Dtos/UserForLoginDto.cs
@@ -2,13 +2,10 @@ using System.ComponentModel.DataAnnotations;
 
 namespace DatinApp.API.Dtos
 {
-    public class UserForRegisterDto
+    public class UserForLoginDto
     {
-        [Required]
         public string Username { get; set; }
-
-        [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must enter more than 4 charecters")]
+        
         public string Password { get; set; }
     }
 }
