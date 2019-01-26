30ea89dd2ff61fb1e9e7506d2d86363454d619a5
bulat <m11e@yandex.ru>
2019-01-26 18:55:55 +0300

    component communication parent to child

--- DatinApp.API/Controllers/ValuesController.cs
+++ DatinApp.API/Controllers/ValuesController.cs
@@ -23,6 +23,7 @@ namespace Dataing.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
