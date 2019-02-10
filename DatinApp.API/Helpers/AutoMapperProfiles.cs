5b20a6b1ee917b5e33dc1cf4ed4aab7aa6d2cdee
bulat <m11e@yandex.ru>
2019-02-10 21:13:29 +0300

    user update  controller added

--- DatinApp.API/Helpers/AutoMapperProfiles.cs
+++ DatinApp.API/Helpers/AutoMapperProfiles.cs
@@ -24,6 +24,7 @@ namespace DatinApp.API.Helpers
                     opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotoForDetailedDto>();
+            CreateMap<UserForUpdateDto, User>();
         }
     }
 }
