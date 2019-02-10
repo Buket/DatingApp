323d03b87b32d81018efb71a71d5480b2c6f5a7b
bulat <m11e@yandex.ru>
2019-02-10 14:51:31 +0300

    member-edit

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -9,6 +9,8 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -21,6 +23,8 @@ export const appRoutes: Routes = [
                 resolve: { users: MemberListResolver } },
             { path: 'members/:id', component: MemberDetailComponent,
                 resolve: { user: MemberDetailResolver } },
+            { path: 'member/edit', component: MemberEditComponent,
+                resolve: { user: MemberEditResolver} },
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent },
         ]
