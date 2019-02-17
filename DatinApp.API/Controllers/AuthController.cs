a5b3a6d588178ff44dd982c517cbf8ebbfda2f1a
bulat <m11e@yandex.ru>
2019-02-17 10:38:00 +0300

    extending register api

--- DatinApp.API/Controllers/AuthController.cs
+++ DatinApp.API/Controllers/AuthController.cs
@@ -36,14 +36,13 @@ namespace DatinApp.API.Controllers
 
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("username alredy exists");
-            var userToCreate = new User
-            {
-                Username = userForRegisterDto.Username  
-            };
+            var userToCreate = _mapper.Map<User>(userForRegisterDto);
 
             var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
 
-            return StatusCode(201);
+            var userToReturn = _mapper.Map<UserForDetailedDto>(createdUser);
+
+            return CreatedAtRoute("GetUser", new { controller = "Users", id = createdUser.Id }, userToReturn);
         }
 
         [HttpPost("login")]
