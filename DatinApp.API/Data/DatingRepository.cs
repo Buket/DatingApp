4a70e79baf69865a3a2269d8157a0dfc5eeab3c4
bulat <m11e@yandex.ru>
2019-02-17 23:25:27 +0300

    api getUser pagination

--- DatinApp.API/Data/DatingRepository.cs
+++ DatinApp.API/Data/DatingRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatinApp.API.Helpers;
 using DatinApp.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -42,10 +43,10 @@ namespace DatinApp.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.UsersSet.Include(p => p.Photos).ToListAsync();
-            return users;
+            var users =  _context.UsersSet.Include(p => p.Photos);
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
